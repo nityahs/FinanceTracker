2b0dd6b349260e57241efe99ecf3058642f9caa1
nityahs <148626044+nityahs@users.noreply.github.com>
2025-03-03 12:59:54 +0530

    update ports

--- Budget/src/main/java/com/example/Budget/BudgetApplication.java
+++ Budget/src/main/java/com/example/Budget/BudgetApplication.java
@@ -2,6 +2,8 @@ package com.example.Budget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BudgetApplication {
@@ -9,5 +11,8 @@ public class BudgetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
