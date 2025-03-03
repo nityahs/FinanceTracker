80d2a2bb417fbbcaf0fdaee287f0cead55916244
nityahs <148626044+nityahs@users.noreply.github.com>
2025-03-03 13:31:18 +0530

    add maven

--- homepage/src/main/java/com/example/homepage/HomepageApplication.java
+++ homepage/src/main/java/com/example/homepage/HomepageApplication.java
@@ -2,6 +2,8 @@ package com.example.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HomepageApplication {
@@ -9,5 +11,8 @@ public class HomepageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomepageApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
