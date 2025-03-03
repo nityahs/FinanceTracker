10a61e36c5dcb071db496e51d595db7c3bc30215
nityahs <148626044+nityahs@users.noreply.github.com>
2025-03-03 13:43:14 +0530

    microservice comm establishment

--- signup/src/main/java/com/example/signup/SignupApplication.java
+++ signup/src/main/java/com/example/signup/SignupApplication.java
@@ -2,16 +2,18 @@ package com.example.signup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SignupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SignupApplication.class, args);
-		@Bean
-		public RestTemplate restTemplate() {
-			return new RestTemplate();
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SignupApplication.class, args);
+    }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
